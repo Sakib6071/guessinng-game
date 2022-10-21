bde2a5d69cd0cc598160bbe3ba082d9a4f191a33
Your Name <you@example.com>
2022-10-22 03:10:26 +0600

    initially project done

--- logic/app.js
+++ logic/app.js
@@ -1,2 +1,43 @@
+const random = Math.round(Math.random()*4+1)
+console.log(random);
+let count = 3;
+
+    document.getElementById("btn").addEventListener('click',function(){
+        count=count-1;
+        
+        if(count>=0){
+            document.getElementById('life').innerText=count;
+            const inputField=document.getElementById('inputField')
+            const inputValue = inputField.value;
+            const inputNumber = Number.parseInt(inputValue)
+            console.log(inputNumber);
+            // console.log(typeof(inputNumber));
+            inputField.value=''
+        
+            if((inputNumber <= 5) && (inputNumber>=1)){
+                if(random == inputNumber){
+                    document.getElementById('message-field').innerText="Yahoo!! You are correct"
+                    document.getElementById('message-field').classList.add('text-primary') 
+                }else{
+                    document.getElementById('message-field').innerText="Oh ho!! You are wrong"
+                    document.getElementById('message-field').classList.add('text-danger') 
+
+                }
+        
+            }else{
+            
+            document.getElementById('message-field').innerText="Sorry!Please select a valid number"
+            document.getElementById('message-field').classList.add('text-danger')
+        
+            }
+        }else{
+            document.getElementById('message-field').innerText="Sorry! You Have No Life"
+            document.getElementById('message-field').classList.add('text-danger') 
+        }
+
+
+        
+        })
+
 
 
