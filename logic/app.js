0d5ed18d39182d78cc22fa57431e687f3346c187
Your Name <you@example.com>
2022-10-22 03:32:28 +0600

    correction color and font size for all devices

--- logic/app.js
+++ logic/app.js
@@ -4,22 +4,22 @@ let count = 3;
 
     document.getElementById("btn").addEventListener('click',function(){
         count=count-1;
-        
+        const inputField=document.getElementById('inputField')
+        const inputValue = inputField.value;
+        const inputNumber = Number.parseInt(inputValue)
+        //console.log(inputNumber);
+        // console.log(typeof(inputNumber));
+        inputField.value=''       
         if(count>=0){
             document.getElementById('life').innerText=count;
-            const inputField=document.getElementById('inputField')
-            const inputValue = inputField.value;
-            const inputNumber = Number.parseInt(inputValue)
-            console.log(inputNumber);
-            // console.log(typeof(inputNumber));
-            inputField.value=''
-        
             if((inputNumber <= 5) && (inputNumber>=1)){
                 if(random == inputNumber){
                     document.getElementById('message-field').innerText="Yahoo!! You are correct"
-                    document.getElementById('message-field').classList.add('text-primary') 
+                    document.getElementById('message-field').classList.remove('text-danger') 
+                    document.getElementById('message-field').classList.add('text-success') 
                 }else{
                     document.getElementById('message-field').innerText="Oh ho!! You are wrong"
+                    document.getElementById('message-field').classList.remove('text-success')
                     document.getElementById('message-field').classList.add('text-danger') 
 
                 }
@@ -27,11 +27,13 @@ let count = 3;
             }else{
             
             document.getElementById('message-field').innerText="Sorry!Please select a valid number"
+            document.getElementById('message-field').classList.remove('text-success')
             document.getElementById('message-field').classList.add('text-danger')
         
             }
         }else{
             document.getElementById('message-field').innerText="Sorry! You Have No Life"
+            document.getElementById('message-field').classList.remove('text-success')
             document.getElementById('message-field').classList.add('text-danger') 
         }
 
