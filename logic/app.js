e3043d8c69502d29c89c425c18d9eb4e28e40759
Your Name <you@example.com>
2022-10-22 05:08:39 +0600

    add necessary comment and clean the code

--- logic/app.js
+++ logic/app.js
@@ -1,23 +1,29 @@
+/* restart button functionality */
 document.getElementById('btn-restart').addEventListener('click',function(){
     location.reload()
 })
+
+
+/* random number generate 1 to 5 */
 const random = Math.round(Math.random()*4+1)
-console.log(random);
 let count = 3;
 
+/* Submit button functionality */
     document.getElementById("btn").addEventListener('click',function(){
         count=count-1;
+
+        /* Take input and convert to number */
         const inputField=document.getElementById('inputField')
         const inputValue = inputField.value;
         const inputNumber = Number.parseInt(inputValue)
-        //console.log(inputNumber);
-        // console.log(typeof(inputNumber));
         inputField.value=''       
         if(count>=0){
+            /* update life value */
             document.getElementById('life').innerText=count;
+
+            /* Check input number and random number */
             if((inputNumber <= 5) && (inputNumber>=1)){
-                if(random == inputNumber){
-                    
+                if(random == inputNumber){                   
                     document.getElementById('main-bg').classList.remove('bg-danger')
                     document.getElementById('main-bg').classList.add('bg-success')
                     document.getElementById('message-field').innerText="Yahoo!! You are correct"
